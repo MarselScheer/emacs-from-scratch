db9d761a93cb1f043d2bc34e6257ee10f7f3ba56
Marsel Scheer <scheer@freescience.de>
2024-03-15 17:49:22 +0100

    install straight and gptel

---
+++ early-init.el
@@ -0,0 +1,4 @@
+;; From straight README
+;; Users of Emacs versions >= 27 will want to add:
+(setq package-enable-at-startup nil)
+;; to their early init-file to prevent package.el loading packages prior to their init-file loading.
